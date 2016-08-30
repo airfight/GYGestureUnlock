bfd7a39b1172c4b12eff3b080af441109292a24c
airfight <745756619@qq.com>
2016-08-30 09:04:39 +0800

    //指纹解锁

--- GYGestureUnlock/AppDelegate.swift
+++ GYGestureUnlock/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
+        /*
         let context = LAContext()
         var autherror: NSError?
         var errorReason = "Secret"
@@ -70,7 +71,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             //不支持指纹识别
            print(autherror?.localizedDescription)
         }
-        
+        */
         
         return true
     }
